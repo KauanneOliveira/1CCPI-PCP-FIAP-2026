1b4f44c3e5f51043e66ed4154bf7da981ca8965a
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-05-10 17:11:54 -0300

    adiciona aula 03 - modulos, funcoes, condicionais

---
+++ aula03-modulos-condicionais/aula/aula03b-funcoes.py
@@ -0,0 +1,9 @@
+# FUNCOES
+#==========================================
+def print_lyrics():
+    print("E foi chocante ver você de novo na minha frente")
+    print("E o impacto de não ser tão diferente")
+
+print_lyrics()              # exibe o conteudo
+print(print_lyrics)         # exibe o local que a funcao esta
+print(type(print_lyrics))
---
+++ aula03-modulos-condicionais/aula/aula03e-match-case.py
@@ -0,0 +1,11 @@
+# MATCH-CASE
+#==========================================
+escolha_usuario = 1
+
+match escolha_usuario:
+    case 0:
+        print("Sair do programa")
+    case 1:
+        print("Entrar no programa")
+    case _:
+        print("Erro!")
