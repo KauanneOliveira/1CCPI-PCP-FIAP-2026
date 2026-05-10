93fce8a9bc8e8b84e41fa9a2dd1b425cd2b061dd
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-05-10 14:16:28 -0300

    adiciona primeira aula

---
+++ PycharmProjects/PythonProject/aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,29 @@
+print("--- PRIMEIROS COMANDOS ---")
+
+print('Olá, Mundo')
+print(7 + 4)
+print('7' + '4')
+
+# comentarios em linha
+"""
+comentarios 
+em multiplas 
+linhas
+"""
+
+# VARIAVEIS
+#==========================================
+nome = 'Iris Pessego'
+idade = 17
+peso = 55.0
+
+print(nome, idade, peso)
+print(f'Oi, {nome}')
+
+# INPUT
+#==========================================
+nome = input('Qual o seu nome?: ')
+idade = input('Quantos anos você tem?: ')
+peso = input('Quanto você pesa?: ')
+
+print(nome, idade, peso)
