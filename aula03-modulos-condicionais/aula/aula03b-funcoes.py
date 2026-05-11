1713a81d67c5f0d2a1037b04eaa2666aae54c874
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-05-11 00:34:41 -0300

    adiciona exemplo

--- aula03-modulos-condicionais/aula/aula03b-funcoes.py
+++ aula03-modulos-condicionais/aula/aula03b-funcoes.py
@@ -7,3 +7,19 @@ def print_lyrics():
 print_lyrics()              # exibe o conteudo
 print(print_lyrics)         # exibe o local que a funcao esta
 print(type(print_lyrics))
+
+print()
+
+def saudar_usuario(nome):
+    return f"Olá, {nome}! Bem-vinda ao curso"
+
+mensagem = saudar_usuario(input('Digite seu nome: '))
+print(mensagem)
+
+escola = "FIAP" # GLOBAL (visível em qualquer lugar)
+
+def minha_aula():
+    materia = "Python" # LOCAL (só existe aqui dentro)
+    print(f"Estudando {materia} na {escola}")
+
+minha_aula()
